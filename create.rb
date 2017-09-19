550e1d316367ea23886469549fea268795a74ef4
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-09-19 20:19:38 +0800

    -m CC

--- create.rb
+++ create.rb
@@ -102,7 +102,7 @@ File.open(time.strftime('%Y%m%d.hw'), 'r').each_line do |line|
   end
 end.close
 
-puts
-puts <<END
+puts unless machine
+puts <<END unless machine
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />本作品采用 <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a> 进行许可。
 END
