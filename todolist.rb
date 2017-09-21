3bbd0befd9d91d2b5642541bbf82c9cf435907e1
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-09-21 20:59:41 +0800

    todolist

--- todolist.rb
+++ todolist.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'json'
+
 ID = {
   'C' => '2161861608',
   'M' => '2161861656',
@@ -7,6 +9,9 @@ ID = {
   'S' => '2161861664',
   'H' => '2161861686'
 }.freeze
+
+date = Time.now.strftime('%u').to_i > 4 && 'Sun' || 'today'
+date.freeze
 
 @class = '2161861537'
 homework = IO.popen './create.rb -m'
@@ -16,7 +21,22 @@ homework.each_line do |line|
     @class = ID[line]
     next
   end
-  line += '" ", \"priority\": 4' if @class == '2161861686' && line =~ /^作业本/
   puts "#{@class}: #{line}"
-  `./todolist.sh #{@class} "#{line}"`
+
+  commands = {
+    type: 'item_add',
+    temp_id: `uuidgen`.strip,
+    uuid: `uuidgen`.strip,
+    args: {
+      project_id: @class,
+      content: line,
+      date_string: date
+    }
+  }
+
+  commands[:args]['priority'] = 4 if @class == ID['H'] && line =~ /^《作业本/
+
+  `curl 'https://todoist.com/api/v7/sync' \
+    -d token=65062a4daa6bf9a362447187dacf8af40f56617b \
+    -d commands='[#{commands.to_json}]'`
 end
