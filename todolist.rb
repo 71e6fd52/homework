d8c13955b5b37f44e80b998534560f7fc88653b9
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-09-27 19:19:31 +0800

    todolist delete

--- todolist.rb
+++ todolist.rb
@@ -7,19 +7,42 @@ ID = {
   'M' => '2161861656',
   'E' => '2161861617',
   'S' => '2161861664',
-  'H' => '2161861686'
+  'H' => '2161861686',
+  'N' => '2161861537'
 }.freeze
+
+delete = []
+ID.each_value do |id|
+  items = JSON.parse `curl https://todoist.com/api/v7/projects/get_data \
+    -d token=65062a4daa6bf9a362447187dacf8af40f56617b \
+    -d project_id=#{id}`
+
+  items['items'].each do |item|
+    delete << item['id']
+  end
+end
+
+commands = [{
+  type: 'item_delete',
+  uuid: `uuidgen`.strip,
+  args: { ids: delete }
+}]
+
+puts commands.to_json
+`curl 'https://todoist.com/api/v7/sync' \
+  -d token=65062a4daa6bf9a362447187dacf8af40f56617b \
+  -d commands='#{commands.to_json}'`
 
 date = Time.now.strftime('%u').to_i > 4 && 'Sun' || 'today'
 date.freeze
 
 commands = []
-subject = '2161861537'
+subject = ID['N']
 
 homework = IO.popen './create.rb -m'
 homework.each_line do |line|
   line.chomp!
-  if line =~ /^[A-Z]$/
+  if ID.keys? line
     subject = ID[line]
     next
   end
